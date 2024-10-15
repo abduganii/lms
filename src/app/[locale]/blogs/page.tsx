23bfafeca0c7b5b8f1ba29023bea324e5ca499fb
abduganii <abduganiabdu2@gmail.com>
2024-10-15 16:09:20 +0500

    fixed url

--- src/app/[locale]/blogs/page.tsx
+++ src/app/[locale]/blogs/page.tsx
@@ -9,7 +9,6 @@ async function getBlogsCategories({lang}:any) {
   return fetchData(`${process.env.NEXT_PUBLIC_URL}/blogs-categories/list`,lang);
 }
 
-// /api/blogs-categories/list
 export default async function Blogs({ searchParams }: any) {
   const blogs = await getBlogs({ page: searchParams?.page,category: searchParams?.category,lang:searchParams.locale })
   const categories = await getBlogsCategories({lang:searchParams.locale})
--- src/components/sections/contact.tsx
+++ src/components/sections/contact.tsx
@@ -1,24 +1,61 @@
+"use client"
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
 import Container from "@/components/ui/container";
 import img1 from '@/public/Frame1000001344.png'
 import Image from "next/image";
+import { useState } from 'react';
+import { LoadingIcons } from '../icon';
+import { toast } from 'react-toastify';
 
+type FormData = {
+    name: string;
+    phone: string;
+    email: string;
+    subject: string;
+    message: string;
+};
 
 export default function ContactSections() {
+    const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
+    const [loading,setLoading] = useState(false)
+    const onSubmit = async (data: FormData) => {
+        setLoading(true)
+      try {
+        console.log(data)
+         await axios.post(process.env.NEXT_PUBLIC_URL+'/feedback', data)
+         .then(data=>{
+            toast.success('Form submitted successfully!');
+         })
+         .catch(()=>{
+            toast.error('Failed to submit form');
+         })
+         .finally(()=> setLoading(false))
+     
+      } catch (error) {
+        console.error('Error sending date:', error);
+      }
+    };
     return (
             <Container className='mb-[120px]' >
                 <div className='flex items-center flex-wrap lg:flex-nowrap bg-[#F4F6FB] dark:bg-[#001E45]  rounded-3xl overflow-hidden '>
                     <Image className="w-full lg:max-w-[564px] max-h-[523px]" src={img1} width={564} height={523} alt="img"/>
-                    <form className="w-full inline-block p-4 md:pl-[72px] md:pr-[46px]">
+                    <form  onSubmit={handleSubmit(onSubmit)} className="w-full inline-block p-4 md:pl-[72px] md:pr-[46px]">
                         <h3 className="text-4xl font-semibold leading-[38.73px] text-center mb-[32px]">Связаться с нами</h3>
-                        <input type="text" className="w-full dark:bg-[#001E45] dark:border dark:border-[#00132D] px-[16px] py-[14px] rounded-xl text-sm font-normal leading-6 text-left mb-4" placeholder="ФИО"/> 
+                        <input  {...register('name', { required: 'Name is required' })} type="text" className="w-full dark:bg-[#001E45] dark:border dark:border-[#00132D] px-[16px] py-[14px] rounded-xl text-sm font-normal leading-6 text-left mb-4" placeholder="ФИО"/> 
                         <div className="flex gap-4 mb-4">
-                            <input type="text" className="w-full dark:bg-[#001E45] dark:border dark:border-[#00132D] px-[16px] py-[14px] rounded-xl text-sm font-normal leading-6 text-left" placeholder="Телефон"/> 
-                            <input type="email" className="w-full dark:bg-[#001E45] dark:border dark:border-[#00132D] px-[16px] py-[14px] rounded-xl text-sm font-normal leading-6 text-left" placeholder="Почта"/> 
+                            <input  {...register('phone', { required: 'Phone is required' })} type="text" className="w-full dark:bg-[#001E45] dark:border dark:border-[#00132D] px-[16px] py-[14px] rounded-xl text-sm font-normal leading-6 text-left" placeholder="Телефон"/> 
+                            <input  {...register('email', { required: 'Email is required' })} type="email" className="w-full dark:bg-[#001E45] dark:border dark:border-[#00132D] px-[16px] py-[14px] rounded-xl text-sm font-normal leading-6 text-left" placeholder="Почта"/> 
                         </div>
-                        <input type="text" className="w-full dark:bg-[#001E45] dark:border dark:border-[#00132D] px-[16px] py-[14px] rounded-xl text-sm font-normal leading-6 text-left mb-4" placeholder="Тема"/> 
-                        <input type="text" className="w-full dark:bg-[#001E45] dark:border dark:border-[#00132D] px-[16px] py-[14px] rounded-xl text-sm font-normal leading-6 text-left mb-12" placeholder="Описание"/>
+                        <input  {...register('subject', { required: 'Subject is required' })} type="text" className="w-full dark:bg-[#001E45] dark:border dark:border-[#00132D] px-[16px] py-[14px] rounded-xl text-sm font-normal leading-6 text-left mb-4" placeholder="Тема"/> 
+                        <input  {...register('message', { required: 'Message is required' })} type="text" className="w-full dark:bg-[#001E45] dark:border dark:border-[#00132D] px-[16px] py-[14px] rounded-xl text-sm font-normal leading-6 text-left mb-12" placeholder="Описание"/>
 
-                        <button className="p-[13px] bg-[#13399A] text-white rounded-xl w-full  md:max-w-[240px]">Отправить</button> 
+                        <button disabled={loading} className="flex items-center justify-center  p-[13px] bg-[#13399A] text-white rounded-xl w-full  md:max-w-[240px]">
+                            
+                            {
+                                loading && <LoadingIcons/>
+                            }
+                            Отправить</button> 
                     </form>
                 </div>
             </Container>  
