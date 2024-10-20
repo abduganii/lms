1425aac9b6528c1b0a6101e7712942e740ddcfb1
Abdugani Abduqaxxorov <abduganiabdu2@gmail.com>
2024-10-20 19:38:01 +0500

    fixd

--- src/components/ui/lang.tsx
+++ src/components/ui/lang.tsx
@@ -5,6 +5,7 @@ import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { DownIcons } from '../icon';
 import { LangArr } from '@/muck/data';
+import Link from 'next/link';
 import Image from 'next/image';
 import ru from "@/public/lang/ru.png"
 import uz from "@/public/lang/uz.png"
@@ -16,28 +17,27 @@ const langFlag:any = {
     en:en,
     uz_cyr:uz,
 }
+const redirectToLocale = (locale: any, pathname: string) => {
+    if (!pathname) {
+      return '/';
+    }
+  
+    const pathParts = pathname.split('/');
+  
+    pathParts[1] = locale;
+  
+    const url = pathParts.join('/');
+  
+    return url;
+  };
 export default function Lang() {
     const { i18n,t } = useTranslation();
     const currentLocale = i18n.language;
-    const router = useRouter();
     const currentPathname = usePathname();
+
   const [open, setOpen] = useState(false)
 
-  const handleChange = (e:any) => {
-      const newLocale = e.target.getAttribute("value");
-        setOpen(false)
-        const days = 30;
-        const date = new Date();
-        date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-        const expires = '; expires=' + date.toUTCString();
-        document.cookie = `NEXT_LOCALE=${newLocale};expires=${expires};path=/`;
-      if (newLocale) {
-          router.push(
-              currentPathname.replace(`/${currentLocale}`, `/${newLocale}`)
-          )
-          router.refresh();
-      }
-    };
+
     return (
       <>
         <div className="relative ml-auto md:ml-0 cursor-pointer bg-neutral-50  dark:bg-[#001E45] flex items-center p-3 pr-2 border-r-3 rounded-xl gap-[2px] sm:gap-4"
@@ -55,15 +55,17 @@ export default function Lang() {
         </p>
        
     {
-        open ? <ul className='max-w-[108px] absolute top-12  bg-neutral-50 bg-white dark:bg-[#00132D] dark:text-[#FFFFFF] py-2 sm:py-3 rounded-xl w-full left-0"' onClick={handleChange}>
+        open ? <ul className='max-w-[108px] absolute top-12  bg-neutral-50 bg-white dark:bg-[#00132D] dark:text-[#FFFFFF] py-2 sm:py-3 rounded-xl w-full left-0"'>
             {
-                LangArr.map(e=>(
-                    <li className='flex font-normal text-base p-2   hover:bg-neutral-200' value={e?.value} key={e.id}>
-                        <span className="px-2">
-                        <Image src={langFlag?.[e?.value]} alt='img' width={20} height={20}/>
-                        </span>
-                         {e.label}
-                         </li>
+                            LangArr.map((e,i) => (
+                                <Link key={i} href={redirectToLocale(e?.value, currentPathname)}>
+                                <li className='flex font-normal text-base p-2   hover:bg-neutral-200' value={e?.value} key={e.id}>
+                                    <span className="px-2">
+                                    <Image src={langFlag?.[e?.value]} alt='img' width={20} height={20}/>
+                                    </span>
+                                    {e.label}
+                                    </li>
+                                </Link>
                 ))
             }
         </ul>:""
@@ -73,3 +75,7 @@ export default function Lang() {
       
   )
 }
+
+
+
+
